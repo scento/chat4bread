ef2ab970d0fd340e9bbaef78e2ce869048495f53
Wenzel Pünter <wenzel@puenter.org>
2019-08-05 12:34:28 +0000

    :sparkles: Added DB operations for onboarding.

--- backend/db.go
+++ backend/db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"time"
 )
@@ -14,18 +15,25 @@ type ORM struct {
 
 // GeoJSON defines geometric structures such that we can use MongoDB GIS operations.
 type GeoJSON struct {
-	Type   string `json:"type"`
-	Coords []int  `json:"coordinates"`
+	Type   string `bson:"type"`
+	Coords []float64  `bson:"coordinates"`
+}
+
+// MakeGeoJSONPnt creates a new GeoJSON point.
+func MakeGeoJSONPnt(lat float64, lon float64) GeoJSON {
+	return GeoJSON{Type: "Point", Coords: []float64{lon, lat}}
 }
 
 // User object bundling all relevant information about an user.
 type User struct {
-	Phone    string   `json:"phone"`
-	Name     *string  `json:"name"`
-	Location *GeoJSON `json:"location"`
+	ID       primitive.ObjectID `bson:"_id"`
+	Phone    string             `bson:"phone"`
+	Name     *string            `bson:"name"`
+	Location *GeoJSON           `bson:"location"`
+	Kind 	 *string 			`bson:"kind"`
 
-	Action string   `json:"action"`
-	Reqs   []string `json:"requirements"`
+	Action string   `bson:"action"`
+	Reqs   []string `bson:"requirements"`
 }
 
 // NewORM initializes the ORM.
@@ -53,6 +61,48 @@ func (orm *ORM) NewUser(phone string) error {
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	users := orm.DB.Collection("users")
 	_, err := users.InsertOne(ctx, bson.M{"phone": phone,
-		"action": "welcome", "requirements": []string{"name", "location", "type"}})
+		"action": "onboarding", "requirements": []string{"name", "location", "type"}})
+	return err
+}
+
+// ResetUserState resets the user state.
+func (orm *ORM) ResetUserState(user *User) error {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	users := orm.DB.Collection("users")
+	_, err := users.UpdateOne(ctx, bson.M{"_id": user.ID}, bson.M{"$set": bson.M{"action": "",
+		"requirements": []string{}}})
+	return err
+}
+
+// SetUserName sets the name of the user.
+func (orm *ORM) SetUserName(user *User, name string) error {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	users := orm.DB.Collection("users")
+	_, err := users.UpdateOne(ctx, bson.M{"_id": user.ID}, bson.M{"$set": bson.M{"name": name}})
+	return err
+}
+
+// SetUserLocation sets the location of the user.
+func (orm *ORM) SetUserLocation(user *User, lat float64, lng float64) error {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	users := orm.DB.Collection("users")
+	_, err := users.UpdateOne(ctx, bson.M{"_id": user.ID}, bson.M{"$set":
+		bson.M{"location": MakeGeoJSONPnt(lat, lng)}})
+	return err
+}
+
+// SetUserKind sets the type of the user.
+func (orm *ORM) SetUserKind(user *User, kind string) error {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	users := orm.DB.Collection("users")
+	_, err := users.UpdateOne(ctx, bson.M{"_id": user.ID}, bson.M{"$set": bson.M{"kind": kind}})
+	return err
+}
+
+// PopRequirement removes the top requirement of the current action.
+func (orm *ORM) PopRequirement(user *User) error {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	users := orm.DB.Collection("users")
+	_, err := users.UpdateOne(ctx, bson.M{"_id": user.ID}, bson.M{"$pop": bson.M{"requirements": -1}})
 	return err
 }
